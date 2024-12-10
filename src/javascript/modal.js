f11333937aa513bfb9b5935aee1324cace6cbbd0
zeenee <dbwls982@gmail.com>
2024-12-10 14:56:05 +0900

    📝 Edit: 참가 신청 안내 수정

--- src/javascript/modal.js
+++ src/javascript/modal.js
@@ -1,30 +1,39 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const modalSection = document.querySelector(".modal-section");
-  const closeModalButton = document.querySelector(".close-modal");
-  const modalContents = document.querySelector(".modal-contents");
+document.addEventListener('DOMContentLoaded', () => {
+  const modalSection = document.querySelector('.modal-section');
+  const closeModalButton = document.querySelector('.close-modal');
+  const modalContents = document.querySelector('.modal-contents');
+  const openModalButton = document.querySelector('.open-modal');
 
   const closeModal = () => {
-    modalSection.classList.remove("active");
+    modalSection.classList.remove('active');
   };
 
-  modalSection.addEventListener("click", (event) => {
+  const openModal = () => {
+    modalSection.classList.add('active');
+  };
+
+  modalSection.addEventListener('click', (event) => {
     if (event.target === modalSection) {
       closeModal();
     }
   });
 
-  closeModalButton.addEventListener("click", () => {
+  closeModalButton.addEventListener('click', () => {
     closeModal();
   });
 
-  modalContents.addEventListener("click", (event) => {
+  openModalButton.addEventListener('click', () => {
+    openModal();
+  });
+
+  modalContents.addEventListener('click', (event) => {
     if (event.target !== closeModalButton) {
       event.stopPropagation();
     }
   });
 
-  document.addEventListener("keydown", (event) => {
-    if (event.key === "Escape") {
+  document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
       closeModal();
     }
   });
